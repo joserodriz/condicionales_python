de39cde9a062d414e311ef2575ee21987b3b3439
joserodriz <109117839+joserodriz@users.noreply.github.com>
2022-07-26 23:33:07 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -23,9 +23,22 @@ numero_2 = -2
 #       --> En caso afirmativo imprima en pantalla "Resp=3"
 #       --> En caso negativo imprima en pantalla "Resp=4"
 
+if(numero_1 > 5):
+    if(numero_2 > 0):
+        print('Resp=1')
+    
+    else:
+        print('Resp=2')
+else:
+    if(numero_2 > 5):
+        print('Resp=3')
+    else:
+        print('Resp=4')
+
+
 # Verifique la calificación de un estudiante según su
 # puntaje en un examen
-puntaje = 70
+puntaje = 89
 
 # Si el puntaje es mayor igual a 90 --> imprimir A
 # Si el puntaje es mayor igual a 80 --> imprimir B
@@ -35,3 +48,24 @@ puntaje = 70
 
 # Debe imprimir en pantalla la calificacion
 # Utilizar "if" anidados
+
+if(puntaje < 60 ):
+    print('F')
+else:
+    if(puntaje >= 60 and puntaje < 70 ):
+            print('D')
+
+    elif(puntaje >= 70 and puntaje < 80):
+            print('C')
+
+    else:
+        if(puntaje >= 80 and puntaje < 90):
+            print('B')
+
+        elif(puntaje >= 90):
+            print('A')
+
+
+
+
+
