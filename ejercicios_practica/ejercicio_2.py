cf6cd6a2b926121fc8b72a3335ca68d9dda6af73
joserodriz <109117839+joserodriz@users.noreply.github.com>
2022-07-26 23:32:46 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -34,3 +34,32 @@ copia_texto_1 = texto_1  # Copia de la variable texto_1
 
 # Verifique que copia_texto_1 es distinta a texto_2
 # Imprima en pantalla según corresponda
+
+if(texto_1 > texto_2):
+    print('primera palabra {} es mayor a segunda palabra {}'.format(texto_1, texto_2))
+
+else:
+    print('segunda palabra {} es mayor a primera palabra {}'.format(texto_2, texto_1))
+
+if(len(texto_1) > len(texto_2)):
+    print('primera palabra {} tiene mas caracteres que segunda palabra {}'.format(texto_1, texto_2))
+
+else:
+    print('segunda palabra {} tiene mas caracteres que primera palabra {}'.format(texto_2, texto_1))
+
+if(texto_1[0] > texto_2[0]):
+    print('letra {} es mayor a letra {}'.format(texto_1[0], texto_2[0]))
+
+else:
+    print('letra {} es mayor a letra {}'.format(texto_2[0], texto_1[0]))
+
+if(copia_texto_1 == texto_1):
+    print('{} es la copia de primer palabra'.format(copia_texto_1))
+
+else:
+    print('{} No es la copia de la primer palabra'.format(copia_texto_1))
+
+if(copia_texto_1 != texto_2):
+    print('{} no es la copia de segunda palabra'.format(copia_texto_1))  
+
+
